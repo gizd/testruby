dc6e9ac230d98ce72093eea9b53677d591886edf
admin <admin@uuk.hosted-at.thebunker.net>
2012-07-30 19:59:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrkt::Application.config.secret_token = 'cc7a0fa44c165a46d9b84abf4f4e75db2b81dad0e7890a99011131dd12fa424ddfc1773ce353a08b0a874d6a5a1875214ffe4a8c83cbf2c966c1d6cefd25d46d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mrkt::Application.config.session_store :cookie_store, :key => '_mrkt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mrkt::Application.config.session_store :active_record_store
